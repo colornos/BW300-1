e571c284f91fad88d6fc4858ac4dbb0959ec3b80
colornos <rivas432@hotmail.com>
2023-05-08 17:25:04 -0500

    Update BW300plugintemplate2.py

--- plugins/BW300plugintemplate2.py
+++ plugins/BW300plugintemplate2.py
@@ -2,10 +2,13 @@
 # -*- coding: utf8 -*-
 import sys
 import logging
-from ConfigParser import SafeConfigParser
+from configparser import ConfigParser
 import os
-import thread
-import urllib2, urllib
+import threading
+import urllib3
+import urllib.parse
+
+http = urllib3.PoolManager()
 
 class Plugin:
 
@@ -13,53 +16,41 @@ class Plugin:
         return
 
     def execute(self, config, heartratedata):
- #       self.heartratedata = heartratedata
+        # self.temperaturedata = temperaturedata
         # --- part of plugin skeleton
         log = logging.getLogger(__name__)
         log.info('Starting plugin: ' + __name__)
-        #read ini file from same location as plugin resides, named [pluginname].ini
+        # read ini file from same location as plugin resides, named [pluginname].ini
         configfile = os.path.dirname(os.path.realpath(__file__)) + '/' + __name__ + '.ini'
-        pluginconfig = SafeConfigParser()
+        pluginconfig = ConfigParser()
         pluginconfig.read(configfile)
         log.info('ini read from: ' + configfile)
-        
         # --- start plugin specifics here
-
-	device = '104019001'
-	f1=open("one.txt", "r")
-    	if f1.mode == 'r':
+        device = '104019001'
+        f1 = open("rfid.txt", "r")
+        if f1.mode == 'r':
             contents1 = f1.read()
 
-        f2=open("two.txt", "r")
+        f2 = open("pin.txt", "r")
         if f2.mode == 'r':
             contents2 = f2.read()
 
-        f3=open("three.txt", "r")
-        if f3.mode == 'r':
-            contents3 = f3.read()
+        rfid = str(contents1)
+        pin = str(contents2)
 
-        f4=open("four.txt", "r")
-        if f4.mode == 'r':
-            contents4 = f4.read()
+        if rfid == '0':
+            print("No card detected!")
 
-	byte1 = str(contents1)
-	byte2 = str(contents2)
-	byte3 = str(contents3)
-	byte4 = str(contents4)
-
-	if (byte1 == 0) and (byte2 == 0) and (byte3 == 0) and (byte4 == 0):
-	    print "No card detected!"
-	else:
-
+        else:
 	    systolic = heartratedata[0]['systolic']
 	    diastolic = heartratedata[0]['diastolic']
             pulse = heartratedata[0]['pulse']
-	    mydata=[('device', device),('byte1', byte1),('byte2', byte2),('byte3', byte3),('byte4', byte4),('one', systolic),('two', diastolic),('three', pulse)]   #The first is the var name the second is the value
-	    mydata=urllib.urlencode(mydata)
-	    path='http://colornos.com/sensors/helloworld3.php'    #the url you want to POST to
-	    req=urllib2.Request(path, mydata)
-	    req.add_header("Content-type", "application/x-www-form-urlencoded")
-	    page=urllib2.urlopen(req).read()
-	    print page
-
-	    log.info('Finished plugin: ' + __name__)
+            headers = {
+                'User-Agent': 'RaspberryPi/MBP70.py',
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+            form_data = {'rfid': rfid, 'pin': pin, 'one': systolic, 'two': diastolic, 'three': pulse}
+            encoded_data = urllib.parse.urlencode(form_data)
+            r = http.request('POST', 'https://colornos.com/sensors/bp_hr.php', body=encoded_data, headers=headers)
+            print(r.data)
+            log.info('Finished plugin: ' + __name__)
